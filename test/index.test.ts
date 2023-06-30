25d0b81e2c84b51d25d62f0e3af990f049eb304c
Yuki Takei <yuki@weseek.co.jp>
2023-06-30 14:08:45 +0000

    update structure

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,125 +1,36 @@
-import fs from 'node:fs';
 import path from 'node:path';
-import { promisify } from 'util';
 
-import pkg from '../package.json';
+// import { scanAllTemplateStatus, validateTemplatePluginPackageJson, validateTemplatePlugin } from '@growi/pluginkit/dist/v4/server';
 
 
-describe('package.json', () => {
+const projectDirRoot = path.resolve(__dirname, '../');
 
-  it('contains a growiPlugin directive', () => {
-    // when
-    const { growiPlugin } = pkg;
 
-    // then
-    expect(growiPlugin).not.toBeNull();
-  });
+it('Validation for package.json should be passed', () => {
 
-  describe('growiPlugin directive', () => {
+  // // when
+  // const caller = () => validateTemplatePluginPackageJson(projectDirRoot);
 
-    it('contains a directives for template type plugin', () => {
-      // when
-      const { types } = pkg.growiPlugin;
-
-      // then
-      expect(types).not.toBeNull();
-      expect(types.includes('template')).toBeTruthy();
-    });
-
-    it('contains a locales directive', () => {
-      // when
-      const { locales } = pkg.growiPlugin;
-
-      // then
-      expect(locales.length).toBeGreaterThan(0);
-    });
-
-  });
-
+  // // then
+  // expect(caller).not.toThrow();
 });
 
+it('Scanning the templates ends up with no errors', async() => {
 
-const statAsync = promisify(fs.stat);
+  // // setup
+  // const data = await validateTemplatePluginPackageJson(projectDirRoot);
 
-type TplDirStats = {
-  isTemplateExists: boolean,
-  isMetaDataFileExists: boolean,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  meta?: any,
-}
+  // // when
+  // const caller = () => scanAllTemplateStatus(projectDirRoot, data);
 
-async function getStats(tplDir: string): Promise<TplDirStats> {
-  const markdownPath = path.resolve(tplDir, 'template.md');
-  const statForMarkdown = await statAsync(markdownPath);
-  const isTemplateExists = statForMarkdown.isFile();
+  // // then
+  // expect(caller).not.toThrow();
+});
 
-  const metaDataPath = path.resolve(tplDir, 'meta.json');
-  const statForMetaDataFile = await statAsync(metaDataPath);
-  const isMetaDataFileExists = statForMetaDataFile.isFile();
+it('Validation templates returns true', async() => {
+  // // when
+  // const result = await validateTemplatePlugin(projectDirRoot);
 
-  const result: TplDirStats = {
-    isTemplateExists,
-    isMetaDataFileExists,
-  };
-
-  if (isMetaDataFileExists) {
-    result.meta = await import(metaDataPath);
-  }
-
-  return result;
-}
-
-describe('The dist dir', () => {
-
-  const distDirPath = path.resolve(__dirname, '../dist');
-  const distDirFiles = fs.readdirSync(distDirPath);
-
-  describe.concurrent.each(distDirFiles)("has the template dir '%s'", (tplId) => {
-    const tplRootDirPath = path.resolve(distDirPath, tplId);
-    const localeDirs = fs.readdirSync(tplRootDirPath);
-
-    it.concurrent.each(localeDirs)("and it has the locale dir '%s'", async(locale) => {
-      const tplDir = path.resolve(tplRootDirPath, locale);
-
-      const stats = await getStats(tplDir);
-
-      expect(stats.isTemplateExists).toBeTruthy();
-      expect(stats.isMetaDataFileExists).toBeTruthy();
-      expect(stats.meta?.title).not.toBeNull();
-    });
-
-    const supportingLocales = pkg.growiPlugin.locales;
-
-    it(`and it has locale directories corresponding to at least one of the supporting locale list '${supportingLocales}'`, async() => {
-      const results = [];
-
-      for await (const locale of supportingLocales) {
-        const tplDir = path.resolve(tplRootDirPath, locale);
-
-        try {
-          const {
-            isTemplateExists, isMetaDataFileExists, meta,
-          } = await getStats(tplDir);
-
-          if (!isTemplateExists) throw new Error("'template.md does not exist.");
-          if (!isMetaDataFileExists) throw new Error("'meta.md does not exist.");
-          if (meta?.title == null) throw new Error("'meta.md does not contain the title.");
-
-          results.push({ locale, isValid: true });
-        }
-        catch (err) {
-          results.push({
-            locale,
-            isValid: false,
-            reason: err.message,
-          });
-        }
-      }
-
-      // eslint-disable-next-line no-console
-      console.debug({ results });
-
-      expect(results.some(result => result.isValid)).toBeTruthy();
-    });
-  });
+  // // then
+  // expect(result).toBeTruthy();
 });
